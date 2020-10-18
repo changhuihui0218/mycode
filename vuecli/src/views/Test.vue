ee83bafbed36e4f610452dd13b3506c47d7c25ac
changhuihui <1928572390qq@.com>
2020-10-18 16:14:09 +0800

    vuelic

---
+++ vuecli/src/views/Test.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        haha
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+// 作用域scoped  ？？？视频
+
+</style>
